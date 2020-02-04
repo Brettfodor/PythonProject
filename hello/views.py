f9000054a669f9f119cc5e533b72ba8d28a48b46
Brett Fodor <54860489+Brettfodor@users.noreply.github.com>
2020-02-04 18:35:55 -0500

    working navbar and first migration

--- hello/views.py
+++ hello/views.py
@@ -4,7 +4,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello Django!")
+    return render(request, "hello/home.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
 
 def hello_there(request, name):
     
@@ -16,7 +19,9 @@ def hello_there(request, name):
             'date': datetime.now()
         }
     )
-
+def about(request):
+    return render(request, "hello/about.html")
+    
 print("http://127.0.0.1:8000/hello/VSCode")
 
 
