3d242a6def882042be2a98f21c265c9897878622
Brett Fodor <54860489+Brettfodor@users.noreply.github.com>
2020-02-04 17:05:34 -0500

    first commit

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\bfcap\source\repos\PythonProject\.venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ hello/views.py
@@ -0,0 +1,22 @@
+import re
+from datetime import datetime
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return HttpResponse("Hello Django!")
+
+def hello_there(request, name):
+    
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
+print("http://127.0.0.1:8000/hello/VSCode")
+
+
