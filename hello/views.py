1443dc2053a27425d96e3bea58329e45fc974f8d
Brett Fodor <54860489+Brettfodor@users.noreply.github.com>
2020-02-05 10:53:44 -0500

    Finished Django Tutorial

--- hello/views.py
+++ hello/views.py
@@ -2,9 +2,18 @@ import re
 from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+from django.views.generic import ListView
 
-def home(request):
-    return render(request, "hello/home.html")
+class HomeListView(ListView):
+    """Renders the Home Page"""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
 
 def contact(request):
     return render(request, "hello/contact.html")
@@ -21,6 +30,20 @@ def hello_there(request, name):
     )
 def about(request):
     return render(request, "hello/about.html")
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit= False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
+
     
 print("http://127.0.0.1:8000/hello/VSCode")
 
